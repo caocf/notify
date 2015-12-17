e23da70ad720999e3a7e7baf778bc68a25fd1819
yangchao <yangchao@baidu.com>
2015-12-17 20:57:40 +0800

    add client

--- src/main/java/com/baidu/yangchao/notify/common/conf/GlobalConf.java
+++ src/main/java/com/baidu/yangchao/notify/common/conf/GlobalConf.java
@@ -11,7 +11,5 @@ public class GlobalConf {
     // 每次批量执行最大发送上限
     public static Integer EMAIL_SEND_MAX = 1000;
     public static Integer SMS_SEND_MAX = 1000;
-    // 最大线程池配置
-    public static Integer MAX_THREADPOOL = 10;
 
 }
